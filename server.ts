1920ff73f9c12cb988fc01d76e0f5b12bd12e481
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-04-26 21:13:35 +0300

    asdf

--- server.ts
+++ server.ts
@@ -26,7 +26,6 @@ server.use(
   })
 );
 
-// server.use("/uploads", express.static(path.join(__dirname, "uploads")));
 server.use("/uploads", express.static(path.join(__dirname, "dist/uploads")));
 
 server.use(express.json());
