9ec38f69b267726fb680bc4d2904124f5b7153cc
Audrius <audrius.ausrotas@gmail.com>
2025-04-26 16:24:02 +0300

    Remove dist/ folder from Git tracking

--- dist/modules/response.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (res, success, data, message) => {
-    return res.send({ success, data, message });
-};
