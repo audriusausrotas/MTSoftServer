7a85bb84dd4826b1cb4359b626c63e0c3612eeb4
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-04-27 16:51:36 +0300

    testing last cronjob

--- cronJobs/databaseBackupToAtlas.ts
+++ cronJobs/databaseBackupToAtlas.ts
@@ -3,14 +3,12 @@ import cron from "node-cron";
 import fs from "fs";
 
 export const databaseBackupToAtlas = () => {
-  cron.schedule("55 10 * * *", () => {
+  cron.schedule("54 16 * * *", () => {
     console.log("Starting database backup to Atlas...");
 
     const backupFile = `C:/MTwebsite/mongodbBackups/mongo_backup_${
       new Date().toISOString().split("T")[0]
     }.gz`;
-
-    console.log(backupFile);
 
     if (!fs.existsSync(backupFile)) {
       console.error("Backup file missing! Skipping Atlas sync.");
@@ -21,7 +19,7 @@ export const databaseBackupToAtlas = () => {
     const atlasURI = process.env.MONGODB_URI_REMOTE;
 
     exec(
-      `${mongoRestorePath} --gzip --archive=${backupFile} --nsInclude=yourDatabase.* --uri=${atlasURI}`,
+      `${mongoRestorePath} --gzip --archive=${backupFile} --nsInclude=tvora.* --uri=${atlasURI}`,
       (restoreError, restoreStdout, restoreStderr) => {
         if (restoreError) {
           console.error("Restore to Atlas failed:", restoreStderr);
