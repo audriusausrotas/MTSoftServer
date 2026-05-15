9a36e45afa5b71761d80edca03df108096aa323b
Audrius <audrius@modernitvora.lt>
2026-05-15 21:15:24 +0300

    changing naming in deploy

--- cronJobs/databaseBackup.ts
+++ cronJobs/databaseBackup.ts
@@ -1,97 +1,128 @@
+// import cron from "node-cron";
+// import { exec } from "child_process";
+
+// export const backupDatabase = () => {
+//   cron.schedule("10 4 * * *", () => {
+//     console.log("Running daily MongoDB backup...");
+
+//     const mongoDumpPath = "C:/MTwebsite/mongodb/bin/mongodump.exe";
+//     const gzBackupFile = `C:/MTwebsite/mongodbBackups/mongo_backup_${
+//       new Date().toISOString().split("T")[0]
+//     }.gz`;
+
+//     // Run mongodump to create .gz backup
+//     exec(
+//       `${mongoDumpPath} --gzip --archive=${gzBackupFile} --db=moderniTvora`,
+//       (error, stdout, stderr) => {
+//         if (error) {
+//           console.error("Backup failed:", stderr);
+//         } else {
+//           console.log("Backup completed successfully:", gzBackupFile);
+//         }
+//       },
+//     );
+
+//     // Run mongoexport to create .json backup for collections
+//     const collections = [
+//       "backup",
+//       "bonus",
+//       "clients",
+//       "defaultValues",
+//       // "gates",
+//       "gatePrices",
+//       "installation",
+//       "orders",
+//       "fences",
+//       "potentialClients",
+//       "potentialClientsUnsuscribed",
+//       "production",
+//       "products",
+//       "projects",
+//       "projectsArchived",
+//       "websiteSettings",
+//       "projectsDeleted",
+//       "projectsFinished",
+//       "projectsUnconfirmed",
+//       "projectsVersions",
+//       "schedule",
+//       "selectData",
+//       "suppliers",
+//       "userRights",
+//       "users",
+//     ];
+
+//     collections.forEach((collection) => {
+//       const jsonBackupFile = `C:/MTwebsite/mongodbBackups/json/${collection}_backup_${
+//         new Date().toISOString().split("T")[0]
+//       }.json`;
+
+//       const exportCommand = `"C:/MTwebsite/mongodb/bin/mongoexport.exe" --uri="mongodb://localhost:27017" --db=moderniTvora --collection=${collection} --out="${jsonBackupFile}" --jsonArray`;
+
+//       exec(exportCommand, (error, stdout, stderr) => {
+//         if (error) {
+//           console.error(`Backup failed for collection ${collection}:`, stderr);
+//         } else {
+//           console.log(
+//             `Backup completed successfully for collection ${collection}:`,
+//             jsonBackupFile,
+//           );
+//         }
+//       });
+//     });
+
+//     // Clean up backups older than 7 days json
+//     exec(
+//       `forfiles /p "C:\\MTwebsite\\mongodbBackups\\json" /m *_backup_*.json /d -7 /c "cmd /c del @file"`,
+//       (error, stdout, stderr) => {
+//         if (error) {
+//           console.error("json old backup cleanup failed:", stderr);
+//         } else {
+//           console.log("json old backups deleted.");
+//         }
+//       },
+//     );
+
+//     // Clean up old backups older than 7 days .gz
+//     exec(
+//       `forfiles /p "C:\\MTwebsite\\mongodbBackups" /m mongo_backup_*.gz /d -7 /c "cmd /c del @file"`,
+//       (error, stdout, stderr) => {
+//         if (error) {
+//           console.error(".gz old backup cleanup failed:", stderr);
+//         } else {
+//           console.log(".gz old backups deleted.");
+//         }
+//       },
+//     );
+//   });
+// };
+
 import cron from "node-cron";
 import { exec } from "child_process";
 
-export const backupDatabase = () => {
-  cron.schedule("10 4 * * *", () => {
-    console.log("Running daily MongoDB backup...");
-
-    const mongoDumpPath = "C:/MTwebsite/mongodb/bin/mongodump.exe";
-    const gzBackupFile = `C:/MTwebsite/mongodbBackups/mongo_backup_${
-      new Date().toISOString().split("T")[0]
-    }.gz`;
-
-    // Run mongodump to create .gz backup
-    exec(
-      `${mongoDumpPath} --gzip --archive=${gzBackupFile} --db=moderniTvora`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error("Backup failed:", stderr);
-        } else {
-          console.log("Backup completed successfully:", gzBackupFile);
-        }
-      },
-    );
-
-    // Run mongoexport to create .json backup for collections
-    const collections = [
-      "backup",
-      "bonus",
-      "clients",
-      "defaultValues",
-      // "gates",
-      "gatePrices",
-      "installation",
-      "orders",
-      "fences",
-      "potentialClients",
-      "potentialClientsUnsuscribed",
-      "production",
-      "products",
-      "projects",
-      "projectsArchived",
-      "websiteSettings",
-      "projectsDeleted",
-      "projectsFinished",
-      "projectsUnconfirmed",
-      "projectsVersions",
-      "schedule",
-      "selectData",
-      "suppliers",
-      "userRights",
-      "users",
-    ];
-
-    collections.forEach((collection) => {
-      const jsonBackupFile = `C:/MTwebsite/mongodbBackups/json/${collection}_backup_${
-        new Date().toISOString().split("T")[0]
-      }.json`;
-
-      const exportCommand = `"C:/MTwebsite/mongodb/bin/mongoexport.exe" --uri="mongodb://localhost:27017" --db=moderniTvora --collection=${collection} --out="${jsonBackupFile}" --jsonArray`;
-
-      exec(exportCommand, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Backup failed for collection ${collection}:`, stderr);
-        } else {
-          console.log(
-            `Backup completed successfully for collection ${collection}:`,
-            jsonBackupFile,
-          );
-        }
-      });
+const run = (cmd: string) =>
+  new Promise((resolve, reject) => {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) return reject(stderr);
+      resolve(stdout);
     });
+  });
 
-    // Clean up backups older than 7 days json
-    exec(
-      `forfiles /p "C:\\MTwebsite\\mongodbBackups\\json" /m *_backup_*.json /d -7 /c "cmd /c del @file"`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error("json old backup cleanup failed:", stderr);
-        } else {
-          console.log("json old backups deleted.");
-        }
-      },
-    );
+export const backupDatabase = () => {
+  cron.schedule("10 4 * * *", async () => {
+    console.log("[CRON] MTSoft backup started");
 
-    // Clean up old backups older than 7 days .gz
-    exec(
-      `forfiles /p "C:\\MTwebsite\\mongodbBackups" /m mongo_backup_*.gz /d -7 /c "cmd /c del @file"`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(".gz old backup cleanup failed:", stderr);
-        } else {
-          console.log(".gz old backups deleted.");
-        }
-      },
-    );
+    const date = new Date().toISOString().split("T")[0];
+    const backupDir = "/var/www/mtsoft/backups";
+
+    const gzBackupFile = `${backupDir}/mongo_backup_${date}.gz`;
+
+    try {
+      await run(`mongodump --gzip --archive=${gzBackupFile} --db=moderniTvora`);
+      await run(`find ${backupDir} -name "*.gz" -mtime +7 -delete`);
+
+      console.log("[CRON] MTSoft backup OK:", gzBackupFile);
+    } catch (err) {
+      console.error("[CRON] MTSoft backup failed:", err);
+    }
   });
 };
--- cronJobs/databaseBackupToAtlas.ts
+++ cronJobs/databaseBackupToAtlas.ts
@@ -1,63 +1,91 @@
-import { exec } from "child_process";
+// import { exec } from "child_process";
+// import cron from "node-cron";
+// import fs from "fs";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const databaseBackupToAtlas = () => {
+//   cron.schedule("12 4 * * *", () => {
+//     console.log("Restoring MongoDB backup to Atlas...");
+
+//     const collections = [
+//       "backup",
+//       "bonus",
+//       "clients",
+//       "defaultValues",
+//       // "gates",
+//       "gatePrices",
+//       "installation",
+//       "orders",
+//       "fences",
+//       "potentialClients",
+//       "potentialClientsUnsuscribed",
+//       "production",
+//       "products",
+//       "projects",
+//       "projectsArchived",
+//       "websiteSettings",
+//       "projectsDeleted",
+//       "projectsFinished",
+//       "projectsUnconfirmed",
+//       "projectsVersions",
+//       "schedule",
+//       "selectData",
+//       "suppliers",
+//       "userRights",
+//       "users",
+//     ];
+
+//     const atlasURI = process.env.MONGODB_URI_REMOTE2;
+
+//     collections.forEach((collection) => {
+//       const backupFile = `C:/MTwebsite/mongodbBackups/json/${collection}_backup_${
+//         new Date().toISOString().split("T")[0]
+//       }.json`;
+
+//       if (!fs.existsSync(backupFile)) {
+//         console.error(`Backup file for collection ${collection} missing! Skipping Atlas restore.`);
+//         return;
+//       }
+
+//       const restoreCommand = `"C:/MTwebsite/mongodb/bin/mongoimport.exe" --uri="${atlasURI}" --db=ModerniTvora --collection=${collection} --file="${backupFile}" --jsonArray --drop`;
+
+//       exec(restoreCommand, (error, stdout, stderr) => {
+//         if (error) {
+//           console.error(`Restore failed for collection ${collection}:`, stderr);
+//         } else {
+//           console.log(`Collection ${collection} successfully synced to Atlas!`);
+//         }
+//       });
+//     });
+//   });
+// };
+
 import cron from "node-cron";
-import fs from "fs";
+import { exec } from "child_process";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export const databaseBackupToAtlas = () => {
-  cron.schedule("12 4 * * *", () => {
-    console.log("Restoring MongoDB backup to Atlas...");
+  cron.schedule("12 4 * * *", async () => {
+    console.log("[CRON] MTSoft restoring backup to Atlas...");
 
-    const collections = [
-      "backup",
-      "bonus",
-      "clients",
-      "defaultValues",
-      // "gates",
-      "gatePrices",
-      "installation",
-      "orders",
-      "fences",
-      "potentialClients",
-      "potentialClientsUnsuscribed",
-      "production",
-      "products",
-      "projects",
-      "projectsArchived",
-      "websiteSettings",
-      "projectsDeleted",
-      "projectsFinished",
-      "projectsUnconfirmed",
-      "projectsVersions",
-      "schedule",
-      "selectData",
-      "suppliers",
-      "userRights",
-      "users",
-    ];
+    const date = new Date().toISOString().split("T")[0];
+    const backupDir = "/var/www/mtsoft/backups";
+    const gzBackupFile = `${backupDir}/mongo_backup_${date}.gz`;
 
     const atlasURI = process.env.MONGODB_URI_REMOTE2;
 
-    collections.forEach((collection) => {
-      const backupFile = `C:/MTwebsite/mongodbBackups/json/${collection}_backup_${
-        new Date().toISOString().split("T")[0]
-      }.json`;
+    const restoreCmd = `mongorestore --gzip --archive=${gzBackupFile} --uri="${atlasURI}" --drop`;
 
-      if (!fs.existsSync(backupFile)) {
-        console.error(`Backup file for collection ${collection} missing! Skipping Atlas restore.`);
-        return;
+    exec(restoreCmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error("[CRON] MTSoft restore failed:", stderr);
+      } else {
+        console.log("[CRON] MTSoft Atlas restore OK");
       }
-
-      const restoreCommand = `"C:/MTwebsite/mongodb/bin/mongoimport.exe" --uri="${atlasURI}" --db=ModerniTvora --collection=${collection} --file="${backupFile}" --jsonArray --drop`;
-
-      exec(restoreCommand, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Restore failed for collection ${collection}:`, stderr);
-        } else {
-          console.log(`Collection ${collection} successfully synced to Atlas!`);
-        }
-      });
     });
   });
 };
