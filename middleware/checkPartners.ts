721897768ba7ec666fd268675b7d0968f82932bb
Audrius <audrius@modernitvora.lt>
2026-05-18 23:47:22 +0300

    testing

--- middleware/checkPartners.ts
+++ middleware/checkPartners.ts
@@ -3,6 +3,8 @@ import response from "../modules/response";
 require("dotenv").config();
 
 export default function apiKeyMiddleware(req: Request, res: Response, next: NextFunction) {
+  console.log("is middleware");
+  console.log(req.body);
   try {
     const key = req.headers["x-api-key"];
 
@@ -13,7 +15,8 @@ export default function apiKeyMiddleware(req: Request, res: Response, next: Next
     if (key !== process.env.SV_API_KEY) {
       return response(res, false, null, "API raktas neteisingas");
     }
-
+    console.log("is middleware 2");
+    console.log(req.body);
     next();
   } catch (error) {
     return response(res, false, null, "Server error");
