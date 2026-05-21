294fee0fe27a59b829fe2979bcdf8fb0a01b7151
Audrius <audrius@modernitvora.lt>
2026-05-21 18:22:06 +0300

    checking what i get

--- services/externalServices.ts
+++ services/externalServices.ts
@@ -3,39 +3,36 @@ import { createProjectService } from "./projectService";
 import { getUserById } from "./userServices";
 
 export async function orderFence(body: any) {
-  const { orderData, fenceData } = body;
+  const { data } = body;
+  console.log(data);
+  // const user = await getUserById(data.to._id);
 
-  const user = await getUserById(orderData.to._id);
+  // const estimateData = {
+  //   fences: data.data.fences,
+  //   bindings: data.data.bindings,
+  // };
+  // const calculateEstimateResult = await calculateEstimate(estimateData);
 
-  const estimateData = {
-    calculations: {
-      fences: fenceData.fences,
-      retail: false,
-      units: false,
-    },
-  };
-  const calculateEstimateResult = await calculateEstimate(estimateData);
+  // const fixedData = {
+  //   client: data.data.creator,
+  //   fenceMeasures: data.data.fences,
+  //   results: calculateEstimateResult.results,
+  //   works: calculateEstimateResult.works,
+  //   gates: {},
+  //   totalPrice: calculateEstimateResult.totalPrice,
+  //   totalCost: calculateEstimateResult.totalCost,
+  //   totalProfit: calculateEstimateResult.totalProfit,
+  //   totalMargin: calculateEstimateResult.totalMargin,
+  //   priceVAT: calculateEstimateResult.priceVAT,
+  //   priceWithDiscount: calculateEstimateResult.priceWithDiscount,
+  //   discount: false,
+  //   status: "Naujas Užsakymas",
+  //   advance: 0,
+  //   retail: false,
+  // };
+  // console.log(estimateData);
 
-  const fixedFenceData = {
-    client: fenceData.creator,
-    fenceMeasures: fenceData.fences,
-    results: calculateEstimateResult.results,
-    works: calculateEstimateResult.works,
-    gates: {},
-    totalPrice: calculateEstimateResult.totalPrice,
-    totalCost: calculateEstimateResult.totalCost,
-    totalProfit: calculateEstimateResult.totalProfit,
-    totalMargin: calculateEstimateResult.totalMargin,
-    priceVAT: calculateEstimateResult.priceVAT,
-    priceWithDiscount: calculateEstimateResult.priceWithDiscount,
-    discount: false,
-    status: "Naujas Užsakymas",
-    advance: 0,
-    retail: false,
-  };
-  console.log(estimateData);
+  // const result = await createProjectService(fixedData, user);
 
-  const result = await createProjectService(fixedFenceData, user);
-
-  return { success: true, message: "Fence order received" };
+  // return { success: true, message: "Fence order received" };
 }
