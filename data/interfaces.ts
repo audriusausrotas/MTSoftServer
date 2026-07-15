329ecf6dd02faff131ff47a0a1cd778840b3209a
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2026-07-15 14:44:17 +0300

    updated production search field

--- data/interfaces.ts
+++ data/interfaces.ts
@@ -517,9 +517,9 @@ export interface SelectValues {
   gateTypes: string[];
   fenceColors: string[];
   fenceTypes: string[];
-  retailFenceTypes: string[];
   status: string[];
   accountTypes: string[];
+  productionElements: string[];
 }
 
 export interface DefaultValues {
--- schemas/selectSchema.ts
+++ schemas/selectSchema.ts
@@ -9,9 +9,9 @@ const selectSchema = new mongoose.Schema<SelectValues>({
   gateTypes: { type: [String], required: false, default: [] },
   fenceColors: { type: [String], required: false, default: [] },
   fenceTypes: { type: [String], required: false, default: [] },
-  retailFenceTypes: { type: [String], required: false, default: [] },
   status: { type: [String], required: false, default: [] },
   accountTypes: { type: [String], required: false, default: [] },
+  productionElements: { type: [String], required: false, default: [] },
 });
 
 export default mongoose.model("selectData", selectSchema, "selectData");
